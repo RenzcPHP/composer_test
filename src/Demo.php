bb3753781a82dcbc5f2e1f2744d35973dceb1dba
renzhicai <renzhicai@yibaicloud.com>
2023-01-03 15:16:08 +0800

    提交composer包代码

---
+++ src/Demo.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace Burning\MyFirstComposer;
+
+/**
+ * 
+ * Class Demo
+ * @package Burning\MyFirstComposer
+ */
+class Demo
+{
+    public static function hello()
+    {
+        return "Hello world!";
+    }
+
+}
